c33943fd4d5f11eb3d59c782191f194944f642a9
joseche <joseche@gmail.com>
2015-11-25 20:32:10 -0600

    removed unnecessary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-  def authenticate_token!
-    authenticate_or_request_with_http_token do |token, options|
-      @account = Account.find_by(api_token: token)
-    end
-  end
-
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     end
   end
   helper_method :current_user
-
-  def authenticate_user!
-    redirect_to login_path unless current_user
-  end
 
   def login_required
     if session[:user_id]
