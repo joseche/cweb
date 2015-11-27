8a5109888a33929e3df5bac1dffb8f8e1eec437c
joseche <joseche@gmail.com>
2015-11-26 19:29:28 -0600

    old migrations

--- db/migrate/0004_create_activations.rb
+++
@@ -1,9 +0,0 @@
-class CreateActivations < ActiveRecord::Migration
-  def change
-    create_table :activations do |t|
-      t.string :key
-      t.references :user, index: true, foreign_key: true
-      t.timestamps null: false
-    end
-  end
-end
